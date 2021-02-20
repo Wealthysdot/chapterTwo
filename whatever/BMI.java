c07e447c4f0ed2e57dc6ce01e3a37d88ebc9fe6b
Adedotun <56767464+Wealthysdot@users.noreply.github.com>
2021-02-20 01:43:13 +0100

    Updated BMI with test

---
+++ whatever/BMI.java
@@ -0,0 +1,116 @@
+package whatever;
+
+public class BMI {
+    private double weight;
+    private double height;
+    private double BMIValue;
+    private boolean isNegativeValueEntered;
+
+    public BMI(double weight, double height, double BMIValue) {
+        this.weight = weight;
+        this.height = height;
+        this.BMIValue = BMIValue;
+    }
+
+    public double getWeight() {
+//        System.out.println("Enter weight in Kilogram:");
+        return weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setWeight(double weight) {
+        if(weight < 0){
+            isNegativeValueEntered = true;
+        }
+        else{
+            this.weight= weight;
+            isNegativeValueEntered = false;
+        }
+
+    }
+
+    public void setHeight(double height) {
+        if (height < 0){
+            isNegativeValueEntered = true;
+        }
+        else {
+        this.height = height;
+            isNegativeValueEntered = false;
+
+        }
+    }
+
+    public double calculatingBMI() {
+      double BMIValue =  getWeight() / (getHeight() * getHeight());
+      this.BMIValue = BMIValue;
+      return BMIValue;
+
+    }
+
+    public String checkBMIValue() {
+        String message = " ";
+
+        String bmiString = String.format("%.2f", BMIValue);
+        BMIValue = Double.parseDouble(bmiString);
+
+        if (BMIValue < 18.5 ){
+            message = "Your BMI is " + BMIValue + "You are underweight";
+        }
+        else
+            if (BMIValue > 18.5 && BMIValue < 25){
+                message = "Your BMI is " + BMIValue + "You are normal";
+            }
+            else
+                if(BMIValue > 25 && BMIValue < 30){
+                    message = "Your BMI is" +" "+ BMIValue +" "+ "You are Overweight";
+                }
+                else
+                    if(BMIValue > 30){
+                        message = "Your BMI is " + BMIValue + "You RE Obese";
+                    }
+
+                    return message;
+
+    }
+
+
+
+    public boolean isNegativeValueEntered() {
+        return isNegativeValueEntered;
+    }
+
+    public String getInputErrorMessage() {
+        String messageToReturn = "";
+        if (isNegativeValueEntered == true){
+            messageToReturn = "Please enter a positive number";
+        }
+        return messageToReturn;
+    }
+}
+
+
+//    to check for negative number
+//    public void setDigit(int digit) {
+//        if(digit < 0){
+//            isNegativeValueEntered = true;
+//        }
+//        else {
+//            this.digit = digit;
+//        }
+//        }
+////    to check for negative
+//    public boolean isNegativeValueEntered() {
+//        return isNegativeValueEntered;
+//    }
+//
+//    public String getInputErrorMessage() {
+//        String messageToReturn = "";
+//        if (isNegativeValueEntered == true){
+//            messageToReturn = "Please enter a positive number";
+//        }
+//        return messageToReturn;
+//
+//    }
---
+++ whatever/BMIApp.java
@@ -0,0 +1,39 @@
+package whatever;
+
+import java.util.Scanner;
+
+public class BMIApp {
+    public static void main(String[] args) {
+        BMI newBMI = new BMI(0.0,0.0, 0);
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter weight in kilogram:");
+        double weight = input.nextDouble();
+        newBMI.setWeight(weight);
+        while(newBMI.isNegativeValueEntered()) {
+            System.out.println(newBMI.getInputErrorMessage());
+            System.out.println("Enter weight in kilogram:");
+            weight = input.nextDouble();
+            newBMI.setWeight(weight);
+        }
+
+
+        System.out.println("Enter height in meters:");
+        double height = input.nextDouble();
+        newBMI.setHeight(height);
+        while(newBMI.isNegativeValueEntered()) {
+            System.out.println(newBMI.getInputErrorMessage());
+            System.out.println("Enter height in meters:");
+            height = input.nextDouble();
+            newBMI.setHeight(height);
+        }
+            newBMI.calculatingBMI();
+            System.out.println(newBMI.checkBMIValue());
+
+
+
+
+
+    }
+}
